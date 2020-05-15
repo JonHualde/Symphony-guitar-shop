4ec0e0b246ceff1a1f4d9c08bfff75b4b7765d75
Jon <jonhualde3@gmail.com>
2020-05-15 23:02:03 +0200

    Added auth middleware to the project, token is now auto-generated and deleted when the client logs out

--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const salt = 10;
+const jwt = require('jsonwebtoken');
+
+require('dotenv').config();
 
 const userSchema = mongoose.Schema({
     email: {
@@ -63,7 +66,32 @@ userSchema.pre('save', function(next) {
 })
 
 userSchema.methods.comparePassword = function(candidatePassword, cb){
-    
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch){
+        if(err) return cb(err);
+        cb(null, isMatch)
+    })
+}
+
+userSchema.methods.generateToken = function(cb) {
+    var user = this;
+    var token = jwt.sign(user._id.toHexString(), process.env.SECRET)
+
+    user.token = token;
+    user.save(function(err, user){
+        if(err) return cb(err);
+        cb(null, user);
+    })
+}
+
+userSchema.statics.findByToken = function(token, cb) {
+    var user = this;
+
+    jwt.verify(token, process.env.SECRET, function(err, decode){
+        user.findOne({ "_id": decode, "token": token }, function(err, user) {
+            if(err) return cb(err)
+            cb(null, user)
+        })
+    })
 }
 
 
--- server/server.js
+++ server/server.js
@@ -15,15 +15,29 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 
-//===========================
 //        MODELS
-//===========================
-
 const { User } = require('./models/user.js');
+
+//        MIDDLEWARES
+const { auth } = require('./middleware/auth.js');
 
 //============================
 //         USERS
 //============================
+
+app.get('/api/users/auth', auth, (req, res) => {
+    res.status(200).json({
+        isAdmin: req.user.role === 0 ? false : true,
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        role: req.user.role,
+        cart: req.user.cart,
+        history: req.user.history
+    })
+})
+
 
 app.post('/api/users/register', (req, res) => {
     const user = new User(req.body)
@@ -32,7 +46,6 @@ app.post('/api/users/register', (req, res) => {
         if(err) return res.json({ success: false, err })
         res.status(200).json({
             success: true,
-            userData: doc
         })
     })
 })
@@ -43,12 +56,31 @@ app.post('/api/users/login', (req, res) => {
         if(!user) return res.json({ loginSuccess: false, message:'Auth failed, email not found' });
 
         user.comparePassword(req.body.password, (err, isMatch) => { //isMatch is true if the password matches
+            if(!isMatch) return res.json({ loginSuccess: false, message: 'Wrong password' });
 
+            user.generateToken((err, user) => {
+                if(err) return res.status(400).send(err);
+                res.cookie('w_auth', user.token).status(200).json({
+                    loginSuccess: true
+                })
+            })
         })
     })
 })
 
-
+app.get('/api/user/logout', auth, (req, res) => {
+    
+    User.findOneAndUpdate(
+        { _id: req.user._id },
+        { token: '' },
+        (err, doc) => {
+            if(err) return res.json({ success: false, err });
+            return res.status(200).send({
+                success: true
+            })
+        }
+    )
+})
 
 
 
